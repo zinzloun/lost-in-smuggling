5b95b495a9931254d23b329759d19e99ca4c09ac
Filippo *ZinzlouN* Bersani <filobers@protonmail.com>
2024-06-08 00:05:06 +0200

    Add files via upload

---
+++ h2c/configs/generate-certificates.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $(basename $PWD) == "configs" ]; then
+    PREFIX="."
+else
+    PREFIX="configs"
+fi
+
+# Delete previous
+rm $PREFIX/key.pem $PREFIX/cert.pem 2> /dev/null
+# Delete empty dirs created by docker-compose
+rmdir $PREFIX/key.pem $PREFIX/cert.pem 2>/dev/null
+
+openssl genrsa > $PREFIX/key.pem
+openssl req -new -x509 -key $PREFIX/key.pem -out $PREFIX/cert.pem -days 365 -nodes \
+    -subj "/C=US/ST=Test/L=Test/O=Test/OU=Test/CN=localhost"
